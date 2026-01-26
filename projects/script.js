c465c64565ef700ebc54d9fce4304b53507ef3f4
thitan12 <thierryfahasoavana@gmail.com>
2026-01-26 21:06:06 +0300

    dernier leger changement

--- projects/script.js
+++ projects/script.js
@@ -21,11 +21,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Projets | Tonio Thierry";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            $("#favicon").attr("href", "./assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
+            $("#favicon").attr("href", "./assets/images/favhand.png");
         }
     });
 
